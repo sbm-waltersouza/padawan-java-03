e62ff0f3285f3952969a026bcc00e4859a383796
sbm-waltersouza <169094760+sbm-waltersouza@users.noreply.github.com>
2024-07-03 15:16:07 -0300

    reescrevendo metodos

--- bytebank-herdado/src/Gerente.java
+++ bytebank-herdado/src/Gerente.java
@@ -1,29 +1,22 @@
 
 public class Gerente extends Funcionario {
 
-	private String nome;
-	private String cpf;
-	private double salario;
 	private int senha;
-	
+
 	public void setSenha(int senha) {
 		this.senha = senha;
 	}
-	
-	public boolean autentica(int senha) {	
+
+	public boolean autentica(int senha) {
 		if (this.senha == senha) {
 			return true;
 		} else {
 			return false;
 		}
 	}
-	
-/*	public double getBonificacao() {
-		return this.salario;
-	} */
-	
-	
 
-	
-	
+	public double getBonificacao() {
+		return this.salario;
+	}
+
 }
