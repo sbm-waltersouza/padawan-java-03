93e12494ff75f00071c25a56721641e9da1f9ca6
sbm-waltersouza <169094760+sbm-waltersouza@users.noreply.github.com>
2024-07-04 15:06:55 -0300

    Herança e construtores

--- bytebank-herdado/src/Gerente.java
+++ bytebank-herdado/src/Gerente.java
@@ -1,23 +1,22 @@
 public class Gerente extends Funcionario {
-
+	
 	private int senha;
+
+	public double getBonificacao() {
+		System.out.println("Chamando o método de bonificacao do GERENTE");
+		return super.getSalario();
+	}
 
 	public void setSenha(int senha) {
 		this.senha = senha;
 	}
 
 	public boolean autentica(int senha) {
-		if (this.senha == senha) {
+		if(this.senha == senha){
 			return true;
 		} else {
 			return false;
 		}
 	}
 
-	public double getBonificacao() {
-		return super.getBonificacao() + super.salario;
-	}
-
-
 }
-
