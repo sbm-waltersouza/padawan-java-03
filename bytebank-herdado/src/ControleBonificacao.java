078dbffca0161a5b8d34fa0e11313548055356b2
sbm-waltersouza <169094760+sbm-waltersouza@users.noreply.github.com>
2024-07-05 13:05:10 -0300

    subindo arquivos corretos a pasta src

--- bytebank-herdado/src/ControleBonificacao.java
+++ bytebank-herdado/src/ControleBonificacao.java
@@ -11,4 +11,5 @@ public class ControleBonificacao {
 	public double getSoma() {
 		return soma;
 	}
+
 }
--- bytebank-herdado/src/TesteFuncionario.java
+++ bytebank-herdado/src/TesteFuncionario.java
@@ -3,7 +3,9 @@ public class TesteFuncionario {
 
 	public static void main(String[] args) {
 			
-		Funcionario nico = new Gerente();
+		
+		
+		Gerente nico = new Gerente();
 		nico.setNome("Nico Steppat");
 		nico.setCpf("223355646-9");
 		nico.setSalario(2600.00);
